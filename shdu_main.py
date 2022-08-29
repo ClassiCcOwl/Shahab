f9c0b5e8a392c024c23f83aa2d6c1797e8273b61
ClassiCcOwl <89275038+ClassiCcOwl@users.noreply.github.com>
2022-08-29 17:49:09 +0430

    Update shdu_main.py

--- shdu_main.py
+++ shdu_main.py
@@ -100,37 +100,56 @@ def entekhab_vahed(home_driver):
 
 def lesson_taker(entekhab_vahed_driver):
     driver = entekhab_vahed_driver
-    codes = ["831180","831180"]
+    codes = ["831180"]
     code_input_xpath = '//*[@id="ctl00_CH1_txtGCode"]'
     vahed_id = 'ctl00_CH1_lblSumUnits'
     del_loop = [6 * Keys.BACKSPACE]
     
-      while(True):
-        for lessonCode in codes:
-            print(lessonCode)
-            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, vahed_id)))
-            vaheds_taken_before = int(driver.find_element(By.ID, vahed_id).text)
-            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, code_input_xpath))).send_keys(lessonCode , Keys.ENTER)
+    if len(codes) ==1:
+        spammer(codes, driver)
+        driver.close()
+    else:
+        while(True):
+            for lessonCode in codes:
+                print(lessonCode)
+                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, vahed_id)))
+                vaheds_taken_before = int(driver.find_element(By.ID, vahed_id).text)
+                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, code_input_xpath))).send_keys(lessonCode , Keys.ENTER)
 
-            try:
-                WebDriverWait(driver , 1).until(EC.alert_is_present() )
-                my_alert = driver.switch_to.alert
-                print(my_alert.text)
-                my_alert.accept()
-                WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, code_input_xpath))).send_keys(del_loop)
-            except TimeoutException:
-                driver.refresh()
-            except:
                 try:
-                    WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, vahed_id)))
-                    vaheds_taken_after = int(driver.find_element(By.ID, vahed_id).text)
-                    print(f"vaheds_taken_after:{vaheds_taken_after}  vaheds_taken_before :{vaheds_taken_before}")
-                    if vaheds_taken_after > vaheds_taken_before :
-                        print(lessonCode , 'taken')
+                    WebDriverWait(driver , 1).until(EC.alert_is_present() )
+                    my_alert = driver.switch_to.alert
+                    print(my_alert.text)
+                    my_alert.accept()
                     WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, code_input_xpath))).send_keys(del_loop)
                 except TimeoutException:
                     driver.refresh()
+                except:
+                    try:
+                        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.ID, vahed_id)))
+                        vaheds_taken_after = int(driver.find_element(By.ID, vahed_id).text)
+                        print(f"vaheds_taken_after:{vaheds_taken_after}  vaheds_taken_before :{vaheds_taken_before}")
+                        if vaheds_taken_after > vaheds_taken_before :
+                            print(lessonCode , 'taken')
+                        WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, code_input_xpath))).send_keys(del_loop)
+                    except TimeoutException:
+                        driver.refresh()
             
+def spammer (codes , driver):
+    driver = driver
+    code_input_xpath = '//*[@id="ctl00_CH1_txtGCode"]'
+    vahed_id = 'ctl00_CH1_lblSumUnits'
+    del_loop = [6 * Keys.BACKSPACE]
+
+    while(True):
+        for lessonCode in codes:
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, code_input_xpath))).send_keys(lessonCode , Keys.ENTER)
+            WebDriverWait(driver,20).until(EC.alert_is_present())
+            try:
+                driver.switch_to.alert.accept()
+            except:
+                pass
+            WebDriverWait(driver, 20).until(EC.element_to_be_clickable((By.XPATH, code_input_xpath))).send_keys(del_loop)
 
 def main_for_entekhab():
     home_driver = login()
