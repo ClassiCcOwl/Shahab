5406ca24acbf132074990eaa42912a2b1ad11f4b
ClassiCcOwl <89275038+ClassiCcOwl@users.noreply.github.com>
2022-09-01 03:19:51 -0700

    Update run_first.py

--- run_first.py
+++ run_first.py
@@ -1,5 +1,5 @@
 import os
 file_path = os.path.dirname(os.path.abspath(__file__))
 req_path = os.path.join(file_path,"req.txt")
-os.system("py -m ensurepip --upgrade")
+os.system("python.exe -m pip install --upgrade pip")
 os.system(f"pip install --upgrade --force-reinstall -r {req_path}")
